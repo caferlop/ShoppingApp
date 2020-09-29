cbe4c379a367c65b6bb263bdf96c3427ae349a3a
Carlos Fernandez <cfernandez@facephi.com>
2020-09-29 22:01:59 +0200

    Added Shopping Framework to embed business logic

---
+++ Shopping/Shopping/Shopping.h
@@ -0,0 +1,18 @@
+//
+//  Shopping.h
+//  Shopping
+//
+//  Created by Carlos Fernandez on 29/09/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shopping.
+FOUNDATION_EXPORT double ShoppingVersionNumber;
+
+//! Project version string for Shopping.
+FOUNDATION_EXPORT const unsigned char ShoppingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shopping/PublicHeader.h>
+
+
